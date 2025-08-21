b4c2b1ca1ca7b6caa00a6aa013ff90fdd8b87e1d
Daniel Oluwanifesimi David <111236737+AIDavids@users.noreply.github.com>
2025-08-21 12:52:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 
+import os
+import sys
+import subprocess
+
+# Try to import joblib, otherwise install it
+try:
+    import joblib
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "joblib"])
+    import joblib
+
+
+
 import streamlit as st
 import joblib
 import pandas as pd
